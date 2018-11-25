470a8aade91b1b3b8e7beff88a9fd701acb6b4df
Alexey Kalugin <mraikalugin@gmail.com>
2018-11-25 20:37:14 +0300

    Use algorithm

--- src/reversible_string.cpp
+++ src/reversible_string.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <string>
 
 class ReversibleString {
@@ -7,13 +8,7 @@ public:
     s = _s;
   }
   void Reverse() {
-    size_t upper_bound = s.size() - 1;
-    for (size_t i = 0; i < s.size() / 2; i++) {
-      size_t left = i, right = upper_bound - i;
-      char tmp = s[left];
-      s[left] = s[right];
-      s[right] = tmp;
-    }
+    reverse(begin(s), end(s));
   }
   std::string ToString() const {
     return s;
