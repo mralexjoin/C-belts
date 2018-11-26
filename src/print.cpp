f7b15251466c71043e42bc0006aed50502ef4432
Alexey Kalugin <mraikalugin@gmail.com>
2018-11-26 20:42:31 +0300

    Develop streams

--- src/print.cpp
+++ src/print.cpp
@@ -1,13 +1,13 @@
 #include <fstream>
+#include <iostream>
 #include <string>
 
 int main() {
   std::ifstream input("input.txt");
-  std::ofstream output("output.txt");
 
   std::string line;
   while (getline(input, line))
-    output << line << std::endl;
+    std::cout << line << std::endl;
 
   return 0;
 }
