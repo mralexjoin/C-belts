ca3981e344bab846e9e760771b518cf1f25d1520
Alexey Kalugin <mraikalugin@gmail.com>
2018-11-30 23:26:31 +0300

    Compute mass of blocks

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
+#include <cstdint>
+#include <iostream>
+
 int main() {
+  size_t n;
+  std::cin >> n;
+
+  uint16_t r;
+  std::cin >> r;
+
+  uint64_t mass = 0;
+  for (size_t i = 0; i < n; i++) {
+    uint64_t w, h, d;
+    std::cin >> w >> h >> d;
+    mass += w * h * d * r;
+  }
+
+  std::cout << mass << std::endl;
 
   return 0;
 }
