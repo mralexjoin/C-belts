45dc30ab5432f2dfc0c211742d715cd5c38926dd
Alexey Kalugin <mraikalugin@gmail.com>
2018-12-04 19:59:25 +0300

    Test palindrom

---
+++ src/main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+ostream& operator << (ostream& os, const vector<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class T>
+ostream& operator << (ostream& os, const set<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class K, class V>
+ostream& operator << (ostream& os, const map<K, V>& m) {
+  os << "{";
+  bool first = true;
+  for (const auto& kv : m) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << kv.first << ": " << kv.second;
+  }
+  return os << "}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+  if (t != u) {
+    ostringstream os;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+       os << " hint: " << hint;
+    }
+    throw runtime_error(os.str());
+  }
+}
+
+void Assert(bool b, const string& hint) {
+  AssertEqual(b, true, hint);
+}
+
+class TestRunner {
+public:
+  template <class TestFunc>
+  void RunTest(TestFunc func, const string& test_name) {
+    try {
+      func();
+      cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+      ++fail_count;
+      cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
+    }
+  }
+
+  ~TestRunner() {
+    if (fail_count > 0) {
+      cerr << fail_count << " unit tests failed. Terminate" << endl;
+      exit(1);
+    }
+  }
+
+private:
+  int fail_count = 0;
+};
+
+bool IsPalindrom(const string& str) {
+  size_t half = str.size() / 2;
+  for (size_t i = 0; i < half; i++)
+    if (str[i] != str[str.size() - i - 1])
+      return false;
+  return true;
+}
+
+void TestPalindrom() {
+  const map<string, bool> is_palindroms =
+    {
+     {"", true},
+     {"a", true},
+     {"aa", true},
+     {"aba", true},
+     {" a ", true},
+     {"abcddcb", false},
+     {"bcddcba", false},
+     {"abcdba", false},
+     {"abcbb", false},
+     {" bcb", false},
+     {"bcb ", false},
+     {"bbcba", false}
+    };
+  for (const auto& test : is_palindroms)
+    AssertEqual(IsPalindrom(test.first), test.second, test.first);
+}
+
+int main() {
+  TestRunner runner;
+  runner.RunTest(TestPalindrom, "TestPalindrom");
+  return 0;
+}
