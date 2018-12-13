78a22916f566422c27978214a30e3529320222be
Alexey Kalugin <mraikalugin@gmail.com>
2018-12-13 21:18:10 +0300

    Third refactoring

--- src/professions.cpp
+++ src/professions.cpp
@@ -1,77 +1,111 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 9, 2018
+ *      Author: Anton Dogadaev
+ */
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-class Person {
+class Citizen {
 public:
-    Person(const string& name, const string& role) : Name(name), Role(role) {}
-    virtual void Walk(const string& destination) const {
-        cout << Role << ": " << Name << " walks to: " << destination << "\n";
+    Citizen(const string& name, const string& profession) :
+        _name(name), _profession(profession) {}
+
+    const string GetName() const {
+        return _name;
+    }
+    const string GetProfession() const {
+        return _profession;
     }
 
-    const string Name;
-    const string Role;
+    virtual void Walk(const string& destination) const = 0;
+private:
+    const string _name;
+    const string _profession;
 };
 
-class Student : public Person {
+class Student : public Citizen {
 public:
+
     Student(const string& name, const string& favouriteSong) :
-        Person(name, "Student"), FavouriteSong(favouriteSong) {}
+        Citizen(name, "Student"), _favouriteSong(favouriteSong) {}
 
     void Learn() const {
-        cout << Role << ": " << Name << " learns\n";
+        cout << GetProfession() << ": " << GetName() << " learns" << endl;
     }
 
     void Walk(const string& destination) const override {
-        Person::Walk(destination);
+        cout << GetProfession() << ": " << GetName() << " walks to: "
+             << destination << endl;
         SingSong();
     }
 
     void SingSong() const {
-        cout << Role << ": " << Name << " sings a song: " << FavouriteSong << "\n";
+        cout << GetProfession() << ": " << GetName() << " sings a song: "
+             << _favouriteSong << endl;
     }
 
 private:
-    string FavouriteSong;
+    const string _favouriteSong;
 };
 
 
-class Teacher : public Person {
+class Teacher : public Citizen {
 public:
+
     Teacher(const string& name, const string& subject) :
-        Person(name, "Teacher"), Subject(subject) {}
+        Citizen(name, "Teacher"), _subject(subject) {}
 
     void Teach() const {
-        cout << Role << ": " << Name << " teaches: " << Subject << "\n";
+        cout << GetProfession() << ": " << GetName() << " teaches: "
+             << _subject << endl;
     }
+
+    void Walk(const string& destination) const override {
+        cout << GetProfession() << ": " << GetName() << " walks to: "
+             << destination << endl;
+    }
+
 private:
-    string Subject;
+    const string _subject;
 };
 
 
-class Policeman : public Person {
+class Policeman : public Citizen {
 public:
-    explicit Policeman(const string& name) : Person(name, "Policeman") {}
+    Policeman(const string& name) : Citizen(name, "Policeman") {}
 
-    void Check(const Person& p) const {
-        cout << Role << ": " << Name << " checks " << p.Role << ". "
-            << p.Role << "'s name is: " << p.Name << "\n";
+    void Check(const Citizen& c) const {
+        cout << GetProfession() << ": " << GetName() << " checks "
+             << c.GetProfession() << ". " << c.GetProfession() << "'s name is: "
+             << c.GetName() << endl;
+    }
+
+    void Walk(const string& destination) const override {
+        cout << GetProfession() << ": " << GetName() << " walks to: "
+             << destination << endl;
     }
 };
 
-void VisitPlaces(const Person& p, const vector<string>& places) {
-    for (const auto& e : places) { p.Walk(e); }
+
+void VisitPlaces(const Citizen& c, const vector<string>& places) {
+    for (const auto& p : places) {
+        c.Walk(p);
+    }
 }
+
 
 int main() {
     Teacher t("Jim", "Math");
     Student s("Ann", "We will rock you");
     Policeman p("Bob");
 
-    VisitPlaces(t, { "Moscow", "London" });
+    VisitPlaces(t, {"Moscow", "London"});
     p.Check(s);
-    VisitPlaces(s, { "Moscow", "London" });
+    VisitPlaces(s, {"Moscow", "London"});
     return 0;
 }
