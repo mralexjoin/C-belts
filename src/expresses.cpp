a8bb6685261c40a2794b4b7d056a32f73bbae7ef
Alexey Kalugin <mraikalugin@gmail.com>
2018-12-22 18:58:06 +0300

    Express optimization

---
+++ src/expresses.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class RouteManager {
+public:
+  void AddRoute(int start, int finish) {
+    reachable_lists_[start].insert(finish);
+    reachable_lists_[finish].insert(start);
+  }
+  int FindNearestFinish(int start, int finish) const {
+    int result = abs(start - finish);
+    if (reachable_lists_.count(start) == 0) {
+        return result;
+    }
+    const set<int>& reachable_list = reachable_lists_.at(start);
+    auto high = reachable_list.upper_bound(finish);
+    if (high != reachable_list.end()) {
+      result = min(result, abs(*high - finish));
+    }
+    if (high != reachable_list.begin()) {
+      result = min(result, abs(*(prev(high)) - finish));
+    }
+    return result;
+  }
+private:
+  map<int, set<int>> reachable_lists_;
+};
+
+
+int main() {
+  RouteManager routes;
+
+  int query_count;
+  cin >> query_count;
+
+  for (int query_id = 0; query_id < query_count; ++query_id) {
+    string query_type;
+    cin >> query_type;
+    int start, finish;
+    cin >> start >> finish;
+    if (query_type == "ADD") {
+      routes.AddRoute(start, finish);
+    } else if (query_type == "GO") {
+      cout << routes.FindNearestFinish(start, finish) << "\n";
+    }
+  }
+
+  return 0;
+}
