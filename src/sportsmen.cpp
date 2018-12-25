5a4e0ee65c4173fa04bddb106062b40f0f7b3d68
Alexey Kalugin <mraikalugin@gmail.com>
2018-12-25 21:47:06 +0300

    Try forward_list

--- src/sportsmen.cpp
+++ src/sportsmen.cpp
@@ -1,29 +1,29 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
-#include <vector>
+#include <forward_list>
 
 int main() {
-  const size_t SIZE = 100000;
-
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
   size_t n;
   std::cin >> n;
 
-  std::map<size_t, size_t> indices_by_athletes;
-  std::map<size_t, size_t> athletes_by_indices;
-
+  std::forward_list<size_t> athletes;
   for (size_t i = 0; i < n; i++) {
-    size_t current, next;
-    std::cin >> current >> next;
-
-    size_t follower_index = SIZE;
-    if (indices_by_athletes.count(next) != 0) {
-      follower_index = indices_by_athletes.at(next);
+    size_t current, follower;
+    std::cin >> current >> follower;
+    if (follower == 0) {
+      athletes.push_front(current);
+    }
+    else {
+      athletes.insert_after(
+                            std::find(athletes.begin(), athletes.end(), follower),
+                            current);
     }
   }
+
+  athletes.reverse();
 
   for (const int athlete : athletes) {
     std::cout << athlete << ' ';
