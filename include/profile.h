aa8c2f92eaf140349db9a12c36f787ecc1193a4c
Alexey Kalugin <mraikalugin@gmail.com>
2019-01-26 22:26:48 +0300

    Add using to profiler

--- include/profile.h
+++ include/profile.h
@@ -2,59 +2,59 @@
 
 #include <chrono>
 #include <iostream>
-#include <string>
 #include <sstream>
-
-using namespace std;
-using namespace chrono;
+#include <string>
 
 class LogDuration {
+  using steady_clock = std::chrono::steady_clock;
+  using string = std::string;
+
  public:
-  explicit LogDuration(const std::string& msg = "")
-    : message(msg + ": ")
-    , start(std::chrono::steady_clock::now())
-    {
-    }
+  explicit LogDuration(const string& msg = "")
+      : message(msg + ": "), start(steady_clock::now()) {}
 
   ~LogDuration() {
-    auto finish = std::chrono::steady_clock::now();
+    using namespace std;
+    using namespace chrono;
+    auto finish = steady_clock::now();
     auto dur = finish - start;
-    std::cerr << message
-              << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count()
-              << " ms" << std::endl;
+    cerr << message << duration_cast<milliseconds>(dur).count() << " ms"
+         << std::endl;
   }
+
  private:
-  std::string message;
-  std::chrono::steady_clock::time_point start;
+  string message;
+  steady_clock::time_point start;
 };
 
 struct TotalDuration {
+  using steady_clock = std::chrono::steady_clock;
+  using string = std::string;
   string message;
   steady_clock::duration value;
-  explicit TotalDuration(const string& msg = "") :
-    message(msg + ": "),
-    value(0) {}
+  explicit TotalDuration(const string& msg = "")
+      : message(msg + ": "), value(0) {}
   ~TotalDuration() {
+    using namespace std;
+    using namespace chrono;
     ostringstream os;
-    os << message
-       << duration_cast<milliseconds>(value).count()
-       << " ms" << endl;
+    os << message << duration_cast<milliseconds>(value).count() << " ms"
+       << endl;
     cerr << os.str();
   }
 };
 
-class AddDuration
-{
-public:
-  explicit AddDuration(steady_clock::duration& dest):
-    add_to(dest),
-    start(steady_clock::now()) {}
-  explicit AddDuration(TotalDuration& dest):
-    AddDuration(dest.value) {}
-  ~AddDuration() {
-    add_to += steady_clock::now() - start;
-  }
-private:
+class AddDuration {
+  using steady_clock = std::chrono::steady_clock;
+  using string = std::string;
+
+ public:
+  explicit AddDuration(steady_clock::duration& dest)
+      : add_to(dest), start(steady_clock::now()) {}
+  explicit AddDuration(TotalDuration& dest) : AddDuration(dest.value) {}
+  ~AddDuration() { add_to += steady_clock::now() - start; }
+
+ private:
   steady_clock::duration& add_to;
   steady_clock::time_point start;
 };
@@ -62,8 +62,6 @@ private:
 #define UNIQ_ID_IMPL(lineno) _a_local_var_##lineno
 #define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
 
-#define LOG_DURATION(message)                   \
-  LogDuration UNIQ_ID(__LINE__){message};
+#define LOG_DURATION(message) LogDuration UNIQ_ID(__LINE__){message};
 
-#define ADD_DURATION(value)                     \
-  AddDuration UNIQ_ID(__LINE__){value};
+#define ADD_DURATION(value) AddDuration UNIQ_ID(__LINE__){value};
